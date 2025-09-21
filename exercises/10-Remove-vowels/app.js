7374d41d23df5dde1723b6b540f30976eadf71e1
Boma G <51271912+BOMAURIEL@users.noreply.github.com>
2025-09-21 02:39:22 +0000

    last fixes done

--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,5 +1,17 @@
 // Your code goes here
-
+const rapid = (str) => {
+    let newString = '';
+    str = str.toLowerCase();
+    
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (char !== 'a' && char !== 'e' && char !== 'i' && char !== 'o' && char !== 'u') {
+            newString += char.toUpperCase();
+        }
+    }
+    
+    return newString;
+};
 
 // Work above this line; do not change code below
 let str = "John";
